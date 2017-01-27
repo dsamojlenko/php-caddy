a02df3a07eaa74eb1deaaed3c442c508c30814f8
Dave Samojlenko <dave.samojlenko@ssc-spc.gc.ca>
2017-01-27 13:49:44 -0500

    add some messages

--- cli/Caddy/Caddy.php
+++ cli/Caddy/Caddy.php
@@ -8,17 +8,22 @@ class Caddy
 
         $public_path = realpath(getcwd()) . '\public';
         info('Web root: ' . $public_path);
-        info('Caddy bin: ' . VALET_BIN_PATH);
 
         exec(VALET_BIN_PATH . 'RunHiddenConsole.exe ' . VALET_BIN_PATH . 'caddy.exe -conf Caddyfile -root ' . $public_path);
+
+        info('Success!');
+        info('You may access your site at http://localhost');
+        info('You may access mailhog at http://localhost:8025');
     }
 
     function down()
     {
         info('Caddying down...');
-
+        info('Kill the Caddy');
         exec('taskkill /im caddy.exe');
+        info('Kill the Mailhog');
         exec('taskkill /im mailhog.exe');
+        info('Kill the Php');
         exec('taskkill /im php-cgi.exe');
     }
 }
