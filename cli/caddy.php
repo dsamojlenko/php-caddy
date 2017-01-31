3c6bff52518f8c7ffaa1bdb4f576aa6e1a19bd96
Dave Samojlenko <dave.samojlenko@ssc-spc.gc.ca>
2017-01-31 13:06:17 -0500

    need full namespace for callables

--- cli/caddy.php
+++ cli/caddy.php
@@ -7,7 +7,7 @@ $version = '0.3';
 
 $app = new Application('GCSX Caddy', $version);
 
-$app->command('up', 'UpCommand')->descriptions('Start up the Cadddy services');
-$app->command('down', 'DownCommand')->descriptions('Tear down the Caddy services');
+$app->command('up', 'Caddy\Command\UpCommand')->descriptions('Start up the Cadddy services');
+$app->command('down', 'Caddy\Command\DownCommand')->descriptions('Tear down the Caddy services');
 
 $app->run();
