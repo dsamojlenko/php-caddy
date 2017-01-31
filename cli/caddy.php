0cdc7ca83c2f307397f78f22bc5c8808cff9a933
Dave Samojlenko <dave.samojlenko@ssc-spc.gc.ca>
2017-01-31 11:41:48 -0500

    simpler implementation

--- cli/caddy.php
+++ cli/caddy.php
@@ -1,15 +1,6 @@
 #!/usr/bin/env php
 <?php
 
-/**
- * Load correct autoloader depending on install location.
- */
-if (file_exists(__DIR__.'/../vendor/autoload.php')) {
-    require __DIR__.'/../vendor/autoload.php';
-} else {
-    require __DIR__.'/../../../autoload.php';
-}
-
 use Silly\Application;
 use Illuminate\Container\Container;
 
