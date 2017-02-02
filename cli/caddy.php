9bb21d585faf5387d779f368309359722a57fdee
Dave Samojlenko <dave.samojlenko@ssc-spc.gc.ca>
2017-02-02 14:35:54 -0500

    reorder commands

--- cli/caddy.php
+++ cli/caddy.php
@@ -10,14 +10,14 @@ $app = new Application('PHP Caddy', $version);
 $app->command('install', 'Caddy\Command\InstallCommand')
     ->descriptions('Install the PHP Caddy services');
 
-$app->command('uninstall', 'Caddy\Command\UninstallCommand')
-    ->descriptions('Uninstall the PHP Caddy services');
-
 $app->command('start', 'Caddy\Command\StartCommand')
     ->descriptions('Start up the PHP Cadddy services');
 
 $app->command('stop', 'Caddy\Command\StopCommand')
     ->descriptions('Stop the PHP Caddy services');
+
+$app->command('uninstall', 'Caddy\Command\UninstallCommand')
+    ->descriptions('Uninstall the PHP Caddy services');
 
 $app->command('which', 'Caddy\Command\WhichCommand')
     ->descriptions('Determine which Valet driver serves the current working directory');
