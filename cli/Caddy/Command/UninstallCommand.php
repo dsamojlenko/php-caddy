d29c9d7096315fdcee3303cacc3e47928078a2c4
Dave Samojlenko <dave.samojlenko@ssc-spc.gc.ca>
2017-03-27 12:16:42 -0400

    stop php on uninstall

--- cli/Caddy/Command/UninstallCommand.php
+++ cli/Caddy/Command/UninstallCommand.php
@@ -5,16 +5,18 @@ use Caddy\Filesystem;
 use Caddy\HiddenConsole;
 use Caddy\Mailhog;
 use Caddy\Output;
+use Caddy\PhpCgi;
 
 class UninstallCommand
 {
-    public function __invoke(Filesystem $files, Caddy $caddy, Mailhog $mailhog, HiddenConsole $hiddenConsole)
+    public function __invoke(Filesystem $files, Caddy $caddy, Mailhog $mailhog, PhpCgi $php, HiddenConsole $hiddenConsole)
     {
         Output::info('Removing PHP Caddy...');
 
         $files->ensureDirExists(CADDY_HOME_PATH);
         $files->ensureDirExists(CADDY_BIN_PATH);
 
+        $php->stop();
         $caddy->uninstall();
         $mailhog->uninstall();
         $hiddenConsole->uninstall();
